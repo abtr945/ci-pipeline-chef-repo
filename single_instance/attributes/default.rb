c3f37c8f05dafd10b5c59e72794c72d8701ee2be
An Binh Tran <AnBinh.Tran@nicta.com.au>
2014-10-22 13:38:09 +1100

    add dummy attributes

---
+++ single_instance/attributes/default.rb
@@ -0,0 +1,6 @@
+# Dummy values for attributes.
+# Will be overriden by custom JSON each time the recipe is run
+
+default[:jenkins][:dockerregistry] = "dummy-docker-registry"
+default[:jenkins][:jobname] = "dummy-job-name"
+default[:jenkins][:buildnumber] = "123"
