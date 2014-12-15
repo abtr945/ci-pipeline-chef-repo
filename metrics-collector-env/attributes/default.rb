58d214b4a9ff24ed1b5b0322c968525885c781c0
An Binh Tran <an.tran@unswalumni.com>
2014-12-15 14:26:38 +1100

    Update default.rb

--- metrics-collector-env/attributes/default.rb
+++ metrics-collector-env/attributes/default.rb
@@ -1,5 +1,5 @@
 # Dummy values for attributes.
 # Will be overriden by custom JSON each time the recipe is run
 
-default[:jenkins][:jobname] = "dummy-job-name"
+default[:jenkins][:jobname] = "__DUMMY_JOB_NAME__"
 default[:jenkins][:dockerimage] = "dummy-docker-registry-host:5000/dummy-docker-image:latest"
