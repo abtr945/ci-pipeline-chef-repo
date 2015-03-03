a1c4ae5e6e582bc750bc4bdbab9827b4712a755d
Ubuntu <ubuntu@ip-172-30-1-198.ap-southeast-2.compute.internal>
2015-03-03 03:55:21 +0000

    add common docker-deploy cookbook

---
+++ docker-deploy/attributes/default.rb
@@ -0,0 +1,5 @@
+# Dummy values for deployment attributes (empty string).
+# Will be overriden by custom Chef attributes JSON each time the recipe is run.
+
+default[:jenkins][:jobname] = ""
+default[:jenkins][:dockerimage] = ""
---
+++ docker-deploy/recipes/default.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: docker-deploy
+# Recipe:: default
+#
+# Copyright 2015, An Binh Tran
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Install curl
+package "curl" do
+	action :install
+end
+
+# Install docker if not already installed
+execute "install_docker" do
+	user "root"
+	command "curl -sSL https://get.docker.com/ubuntu/ | sh"
+	not_if "dpkg-query -W docker"
+end
+
+# Start docker daemon if not already started
+service "docker" do
+	supports :status => true
+	action :start
+end
+
+# Check for provided attributes: Jenkins Job name and Docker Image URL
+if (node[:jenkins][:jobname] == "") or (node[:jenkins][:dockerimage] == "")
+
+	# Deployment will fail if attributes not provided
+	log "deployment_attributes_not_provided" do
+		message "Jenkins Job name and/or Docker Image URL not provided!"
+		level :error
+	end
+
+else
+
+	# Stop and remove existing Docker container with older app version
+	script "stop_remove_old_docker_container" do
+		interpreter "bash"
+		user "root"
+		code <<-EOH
+		docker stop #{node[:jenkins][:jobname]}
+		docker rm #{node[:jenkins][:jobname]}
+		EOH
+		only_if "docker ps -a | grep #{node[:jenkins][:jobname]}"
+	end
+
+	# Pull new version of Docker image
+	execute "pull_docker_image" do
+		user "root"
+		command "docker pull #{node[:jenkins][:dockerimage]}"
+	end
+
+	# Spawn new Docker container with newer app version from new image
+	execute "spawn_docker_container_from_new_image" do
+		user "root"
+		command "docker run --name #{node[:jenkins][:jobname]} -p 8080:8080 #{node[:jenkins][:dockerimage]}"
+	end
+
+end
