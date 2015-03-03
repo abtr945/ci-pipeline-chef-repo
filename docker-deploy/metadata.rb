6917949210a139bd8e2481f940058e0a4d73499c
Ubuntu <ubuntu@ip-172-30-1-198.ap-southeast-2.compute.internal>
2015-03-03 04:00:22 +0000

    change cookbook metadata

--- docker-deploy/metadata.rb
+++ docker-deploy/metadata.rb
@@ -1,7 +1,7 @@
 name             'docker-deploy'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'An Binh Tran'
+maintainer_email 'AnBinh.Tran@nicta.com.au'
 license          'All rights reserved'
-description      'Installs/Configures docker-deploy'
+description      'Deploy Docker image as Build Artifact to a Chef-managed cluster in Continuous Deployment pipeline'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
